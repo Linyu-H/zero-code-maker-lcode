bac43006652903ed5124fe0f01c08af7ff1b52ca
lyh <3170047405@qq.com>
2026-06-20 02:39:15 +0800

    firts

---
+++ src/main/java/com/commul/ailcode/AiLcodeApplication.java
@@ -0,0 +1,13 @@
+package com.commul.ailcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commul/ailcode/AiLcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commul.ailcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
